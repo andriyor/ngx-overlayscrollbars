e93cc387c1ce42de7bae4fcd27251dda19bcd490
Andriy Orehov <andriyorehov@gmail.com>
2020-03-11 16:38:28 +0200

    add ngx-overlayscrollbars

---
+++ projects/ngx-overlayscrollbars/src/lib/overlayscrollbars.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {OverlayScrollbarsComponent} from "./overlayscrollbars.component";
+
+@NgModule({
+  declarations: [OverlayScrollbarsComponent],
+  imports: [],
+  exports: [OverlayScrollbarsComponent],
+})
+export class OverlayscrollbarsModule { }
---
+++ projects/ngx-overlayscrollbars/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-overlayscrollbars
+ */
+
+export * from './lib/overlayscrollbars.component';
+export * from './lib/overlayscrollbars.module';
